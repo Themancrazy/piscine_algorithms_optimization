61b7cd33722ccb840e2f5dc5b6e998621e5b6dba
Themancrazy <ajanssen@live.fr>
2020-02-24 22:07:27 -0800

    finally fixed error

--- day00/ex03/searchPriceV3.c
+++ day00/ex03/searchPriceV3.c
@@ -8,7 +8,7 @@ size_t			hash(char *input)
 		hash ^= (*input << 5);
 		input++;
 	}
-	return (hash % 2147483647);
+	return (hash % 95000);
 }
 
 struct s_dict	*dictInit(int capacity)
@@ -17,19 +17,26 @@ struct s_dict	*dictInit(int capacity)
 
 	dictionary->capacity = capacity;
 	dictionary->items = malloc(sizeof(struct s_item*) * capacity);
+	int i = -1;
+	while (++i <  capacity)
+		dictionary->items[i] = NULL;
 	return (dictionary);
 }
 
 int				dictInsert(struct s_dict *dict, char *key, struct s_art *value)
 {
+	struct s_item *tmp;
 	struct s_item *new_node = malloc(sizeof(struct s_item));
 
 	new_node->value = value;
 	new_node->key = key;
 	if (dict->items[hash(key)]) {
-		while (dict->items[hash(key)]) {
-			dict->items[hash(key)] = dict->items[hash(key)]->next;
-		}
+		tmp = dict->items[hash(key)];
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_node;
+		tmp->next->next = NULL;
+		return (1);
 	}
 	dict->items[hash(key)] = new_node;
 	dict->items[hash(key)]->next = NULL;
@@ -40,7 +47,18 @@ struct s_art	*dictSearch(struct s_dict *dict, char* key)
 {
 	if (!dict->items[hash(key)])
 		return NULL;
-	return (dict->items[hash(key)]->value);
+	struct s_item *tmp = dict->items[hash(key)];
+	if (strcmp(tmp->value->name, key) != 0)
+	{
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+			if (strcmp(tmp->value->name, key) == 0)
+				return (tmp->value);
+		}
+		return (NULL);
+	}
+	return (tmp->value);
 }
 
 int				searchPrice(struct s_dict *dict, char *name)
