119badf0984a7a3c6d02f44bd4a7dbd8ff217367
Themancrazy <ajanssen@live.fr>
2020-02-24 22:48:34 -0800

    started but need optimization for comprssion

--- day00/ex05/compress.c
+++ day00/ex05/compress.c
@@ -1,24 +1,68 @@
 #include "header.h"
 
-struct s_dict      *dictInit(int capacity)
+size_t			hash(char *input)
 {
-    struct s_dict *dict = malloc(sizeof(struct s_dict));
-    dict->capacity = capacity;
-    dict->items = malloc(sizeof(struct s_item*) * capacity);
-    return (dict);
+	size_t	hash = 0;
+	while (*input)
+	{
+		hash ^= (*input << 5);
+		input++;
+	}
+	return (hash % 95000);
 }
 
-int                 dictInsert(struct s_dict *dict, char *key, int value)
+struct s_dict	*dictInit(int capacity)
 {
-    
+	struct s_dict *dictionary = malloc(sizeof(struct s_dict)); 
+
+	dictionary->capacity = capacity;
+	dictionary->items = malloc(sizeof(struct s_item*) * capacity);
+	int i = -1;
+	while (++i <  capacity)
+		dictionary->items[i] = NULL;
+	return (dictionary);
 }
 
-int                 dictSearch(struct s_dict *dict, char *key)
+int				dictInsert(struct s_dict *dict, char *key, int value)
 {
+	struct s_item *tmp;
+	struct s_item *new_node = malloc(sizeof(struct s_item));
 
+	new_node->value = value;
+	new_node->key = key;
+	if (dict->items[hash(key)]) {
+		tmp = dict->items[hash(key)];
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_node;
+		tmp->next->next = NULL;
+		return (1);
+	}
+	dict->items[hash(key)] = new_node;
+	dict->items[hash(key)]->next = NULL;
+	return (1);
 }
 
-char                *compress(char *book, struct s_dict *dict)
+int	            *dictSearch(struct s_dict *dict, char* key)
 {
+	if (!dict->items[hash(key)])
+		return NULL;
+	struct s_item *tmp = dict->items[hash(key)];
+	if (strcmp(tmp->key, key) != 0)
+	{
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+			if (strcmp(tmp->key, key) == 0)
+				return (tmp->value);
+		}
+		return (-1);
+	}
+	return (tmp->value);
+}
 
+char *compress(char *book, struct s_dict *dict)
+{
+	char *compressed_book = malloc(sizeof(char) * strlen(book));
+	for
 }
